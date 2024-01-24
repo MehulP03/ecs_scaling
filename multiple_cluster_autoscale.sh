7177fcc80a1bd8e1178c2aab00c040d593d13758
Mehul Patangya <mehuljainind@gmail.com>
2024-01-24 16:01:57 +0530

    updated code according to the specific cluster tags key == ENV && value == demo

--- multiple_cluster_autoscale.sh
+++ multiple_cluster_autoscale.sh
@@ -3,31 +3,45 @@
 scaling(){
     # Get all ECS cluster arn
     clusters=$(aws ecs list-clusters --query "clusterArns" --output text)
+    
     # Iterate all clusters arn one by one
     for cluster in $clusters; do
+
         #Fetch the cluster name by cluster arn
         cluster_name=$(aws ecs describe-clusters --clusters $cluster --query "clusters[0].clusterName" --output text)
+        
         #print the cluster name
         echo "Cluster Name: $cluster_name"
-        #fetch the service arn of the cluter
-        services=$(aws ecs list-services --cluster $cluster --query "serviceArns" --output text)
-        #check the cluster have service or not
-        if [ -n "$services" ]; then
-            echo "Services:"
-            # Iterate all service arn one by one
-            for service in $services; do
-                #fetch the service name by service arn
-                service_name=$(aws ecs describe-services --cluster $cluster --services $service --query "services[0].serviceName" --output text)
-                #aws cli command to upscale the cluster
-                aws ecs update-service --cluster $cluster_name --service $service_name --desired-count $DESIRED_COUNT
-                aws ecs wait services-stable --cluster $cluster_name --services $service_name
-                echo "  - $service_name"
-            done
+
+        #fetch cluster tags
+        cluster_tags=$(aws ecs list-tags-for-resource --resource-arn $cluster --query "tags")
+
+        #apply filter on the tags for the required key pair value
+        filtered_tags=$(aws ecs list-tags-for-resource --resource-arn $cluster --query "tags[?key=='ENV' && value=='demo']" --output text)
+        echo "$filtered_tags"
+
+        variableLength=${#filtered_tags}
+       if [ $variableLength -gt 1 ]; then
+            #fetch the service arn of the cluter
+            services=$(aws ecs list-services --cluster $cluster --query "serviceArns" --output text)
+            #check the cluster have service or not
+            if [ -n "$services" ]; then
+                # Iterate all service arn one by one
+                for service in $services; do
+                    # fetch the service name by service arn
+                    service_name=$(aws ecs describe-services --cluster $cluster --services $service --query "services[0].serviceName" --output text)
+                    #aws cli command to upscale the cluster
+                    aws ecs update-service --cluster $cluster_name --service $service_name --desired-count $DESIRED_COUNT > /dev/null
+                    aws ecs wait services-stable --cluster $cluster_name --services $service_name > /dev/null
+                    echo "  - $service_name"
+                done
+            else
+                echo "No services found in the cluster."
+            fi
+
         else
-            echo "No services found in the cluster."
+            echo "The variable is either empty or has a length of 0."
         fi
-        
-        echo "------------------------"
     done
 }
 
